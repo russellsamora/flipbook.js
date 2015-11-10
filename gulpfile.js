c7acfd0e999aea2b7714bf2cd44c8ea002302330
russellgoldenberg <russellgoldenberg@gmail.com>
2015-11-10 09:12:19 -0500

    gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+
 
 gulp.task('default', function() {
+
+	// serve
+	browserSync.init({
+		server: {
+		    baseDir: './'
+		},
+		notify: false
+	});
+
+	// watch
+	gulp.watch(['flipbook.js', 'index.html'], browserSync.reload);
+});
+
+gulp.task('min', function() {
 	return gulp.src('flipbook.js')
 		.pipe(uglify())
 		.pipe(rename('flipbook.min.js'))
